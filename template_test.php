7621d37201c6fac01bedc67bb012e6effbacda8e
Vadim Korolik <vkorolik@gmail.com>
2015-04-24 13:43:04 -0700

    why no work templates

--- template_test.php
+++ template_test.php
@@ -1,12 +1,15 @@
 <?php
-$layout = "base";
-ob_start();
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	$layout = "base";
+	ob_start();
 ?>
+
 <body>
         <h1> WOWOWOW BODY OF TEMPLATE </h1>
 </body>
-<?php
-$content = ob_get_clean();
 
-("base.php");
+<?php
+	$content = ob_get_clean();
+	require("base.php");
 ?>
