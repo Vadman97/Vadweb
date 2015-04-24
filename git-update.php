05e8227816734cba33dd51dfd7b907be780dcf91
Vadim Korolik <vkorolik@gmail.com>
2015-04-23 19:45:18 -0700

    testing some more

--- git-update.php
+++ git-update.php
@@ -1,9 +1,12 @@
 <?php
 	if ($_SERVER['REQUEST_METHOD'] == "POST")
 	{
-		print_r($_POST);
+		//print_r($_POST);
 		//echo "<br><br><br>";
-		echo shell_exec("git pull origin master");
+		//echo shell_exec("git pull origin master");
+		//echo shell_exec("ls -lsah");
+		exec("git pull origin master 2>&1", $return);
+		print_r($return);
 		//echo shell_exec("ls -lsah");
 		echo "DONE PULLING";
 	}
