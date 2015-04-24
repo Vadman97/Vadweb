390c221d47b5e77dd0d413c077d8a8f747ee9513
Vadim Korolik <vkorolik@gmail.com>
2015-04-24 13:34:39 -0700

    Showing butane something

--- git-update.php
+++ git-update.php
@@ -1,7 +1,7 @@
 <?php
 	if ($_SERVER['REQUEST_METHOD'] == "POST")
 	{
-		//print_r($_POST);
+		print_r($_POST);
 		//echo "<br><br><br>";
 		//echo shell_exec("git pull origin master");
 		//echo shell_exec("ls -lsah");
