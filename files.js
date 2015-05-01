81792c35dc3f67609763507c73b9be27192facf7
Vadim Korolik <vkorolik@gmail.com>
2015-04-30 19:27:16 -0700

    testing paste stuff

--- files.js
+++ files.js
@@ -162,173 +162,69 @@ $(window).scroll(function() {
 });
 
 
-//Define paster object with contructor
-var Paster = function(config) {
-
- for(var key in config){
-  this[key]=config[key];
- }
- 
- this.init();
-};
-
-Paster.prototype.pasteEl=null;
-
-Paster.prototype.init=function() {
-
- var paster = this;
-
- if (window.Clipboard) {
-  //IE11, Chrome, Safari
-  this.pasteEl.onpaste=function(e){
-   paster.handlePaste(paster, e);
+// Created by STRd6
+// MIT License
+// jquery.paste_image_reader.js
+(function($) {
+  var defaults;
+  $.event.fix = (function(originalFix) {
+    return function(event) {
+      event = originalFix.apply(this, arguments);
+      if (event.type.indexOf('copy') === 0 || event.type.indexOf('paste') === 0) {
+        event.clipboardData = event.originalEvent.clipboardData;
+      }
+      return event;
+    };
+  })($.event.fix);
+  defaults = {
+    callback: $.noop,
+    matchType: /image.*/
   };
- } else
- {
-  //On Firefox use the contenteditable div hack
-  this.canvas = document.createElement('canvas');
-  this.pasteCatcher = document.createElement("div");
-  this.pasteCatcher.setAttribute("id", "paste_ff");
-  this.pasteCatcher.setAttribute("contenteditable", "");
-  this.pasteCatcher.style.cssText = 'opacity:0;position:fixed;top:0px;left:0px;';
-  this.pasteCatcher.style.marginLeft = "-20px";
-  document.body.appendChild(this.pasteCatcher);
-
-
-  this.pasteEl.onblur=function(e) {
-   paster.pasteCatcher.focus();
+  return $.fn.pasteImageReader = function(options) {
+    if (typeof options === "function") {
+      options = {
+        callback: options
+      };
+    }
+    options = $.extend({}, defaults, options);
+    return this.each(function() {
+      var $this, element;
+      element = this;
+      $this = $(this);
+      return $this.bind('paste', function(event) {
+        var clipboardData, found;
+        found = false;
+        clipboardData = event.clipboardData;
+        return Array.prototype.forEach.call(clipboardData.types, function(type, i) {
+          var file, reader;
+          if (found) {
+            return;
+          }
+          if (type.match(options.matchType) || clipboardData.items[i].type.match(options.matchType)) {
+            file = clipboardData.items[i].getAsFile();
+            reader = new FileReader();
+            reader.onload = function(evt) {
+              return options.callback.call(element, {
+                dataURL: evt.target.result,
+                event: evt,
+                file: file,
+                name: file.name
+              });
+            };
+            reader.readAsDataURL(file);
+            return found = true;
+          }
+        });
+      });
+    });
   };
-
-  this.pasteCatcher.onpaste=function(e) {
-
-   paster.findImageEl(paster);
-  };
- }
-};
-
- 
-Paster.prototype.dataURItoBlob=function(dataURI, callback) {
-// convert base64 to raw binary data held in a string
-// doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-
- console.log(dataURI);
-
- var byteString = atob(dataURI.split(',')[1]);
-// separate out the mime component
- var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-// write the bytes of the string to an ArrayBuffer
- var ab = new ArrayBuffer(byteString.length);
- var ia = new Uint8Array(ab);
- for (var i = 0; i < byteString.length; i++) {
-  ia[i] = byteString.charCodeAt(i);
- }
-
-// write the ArrayBuffer to a blob, and you're done
-
- return new Blob([ia], {type: mimeString});
-};
-
-Paster.prototype.findImageEl = function(paster) {
-
- if (paster.pasteCatcher.children.length > 0) {
-
-  var dataURI = paster.pasteCatcher.firstElementChild.src;
-  if (dataURI) {
-   if (dataURI.indexOf('base64') === -1) {
-    alert("Sorry, with Firefox you can only paste local screenshots and files. Use Chrome or IE11 if you need paster feature.");
-    return;
-   }
-
-   var file = paster.dataURItoBlob(dataURI);
-   paster.uploadFile(paster, file);
-  }
-
-  paster.pasteCatcher.innerHTML = '';
-
- } else
- {
-  setTimeout(function() {
-   paster.findImageEl(paster);
-  }, 100);
- }
-};
-
-Paster.prototype.processing = false; //some wierd chrome bug makes the paste event fire twice when using javascript prompt for the filename
-
-Paster.prototype.handlePaste = function(paster, e) {
-
- //don't do this twice
- if (paster.processing) {
-  return;
- }
-
- //loop through all clipBoardData items and upload it if it's a file.
- for (var i = 0; i < e.clipboardData.items.length; i++) {
-  var item = e.clipboardData.items[i];
-  if (item.kind === "file") {
-
-   paster.processing = true;
-   e.preventDefault();
-   paster.uploadFile(paster, item.getAsFile());
-  }
- }
-};
+})(jQuery);
 
 
-Paster.prototype.uploadFile = function(paster, file) {
+$("html").pasteImageReader(function(results) 
+{
+  var dataURL, filename;
+  return filename = results.filename, dataURL = results.dataURL, results;
+});
 
- var xhr = new XMLHttpRequest();
-
- //progress logging
- xhr.upload.onprogress = function(e) {
-  var percentComplete = (e.loaded / e.total) * 100;
-
-  console.log(percentComplete);
- };
-
- //called when finished
- xhr.onload = function() {
-  if (xhr.status === 200) {
-   alert("Sucess! Upload completed. PHP response will be put in the textarea.");
-  } else {
-   alert("Error! Upload failed");
-  }
-
-  paster.processing = false;
- };
-
- //error handling
- xhr.onerror = function() {
-  alert("Error! Upload failed. Can not connect to server.");
- };
-
- //trigger a callback when it's successful
- xhr.onreadystatechange = function()
- {
-  if (xhr.readyState === 4 && xhr.status === 200)
-  {
-   if (paster.callback) {
-    paster.callback.call(paster.scope || paster, paster, xhr);
-   }
-  }
- };
-
-
- //prompt for the filename
- var filename = prompt("Please enter the file name", "Pasted image");
-
- if (filename) {
-
-  //upload the file
-  xhr.open("POST", "<?php echo $_SERVER['PHP_SELF']; ?>", {
-   filename: filename,
-   filetype: file.type
-  });
-  
-  //send it as multipart/form-data
-  var formData = new FormData();
-  formData.append("pastedFile", file);
-  xhr.send(formData);
- }
-};
+console.log(dataURL);
